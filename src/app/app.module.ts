c5af5fe33da960a2dbd2a91247a9ea3ad6f89367
Mihael Agopyan <mihael.b.agopyan@gmail.com>
2022-05-05 13:40:25 +0300

    Improved Navigation Bar - now each link is underlined on hover; improved Login and Register buttons; initialized routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,24 +2,32 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { MatIconModule } from '@angular/material/icon';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { Routes, RouterModule, Router } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { FooterComponent } from './components/footer/footer.component';
-import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { NavbarAnonComponent } from './components/navbar-anon/navbar-anon.component';
+import { HomeComponent } from './components/home/home.component';
 
+
+const appRoutes: Routes = [
+  {path: '', component: HomeComponent}
+]
 
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent,
-    NavbarAnonComponent
+    NavbarAnonComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     FontAwesomeModule,
     MatIconModule,
-    NoopAnimationsModule
+    NoopAnimationsModule,
+    RouterModule.forRoot(appRoutes, { enableTracing: true })
   ],
   providers: [],
   bootstrap: [AppComponent]
